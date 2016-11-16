d5f6eaae479088d136e64efb41b989d90fb428f5
Francisco Alvarado Ferllini <Francisco Alvarado Ferllini>
2016-11-16 08:40:26 -0600

    sqliteHandler

---
+++ app/src/main/java/com/example/daniel/proyecto3_bases/HttpConnection.java
@@ -0,0 +1,8 @@
+package com.example.daniel.proyecto3_bases;
+
+/**
+ * Created by ferllini13 on 16/11/2016.
+ */
+
+public class HttpConnection {
+}
---
+++ app/src/main/java/com/example/daniel/proyecto3_bases/SQLiteHandler.java
@@ -0,0 +1,75 @@
+package com.example.daniel.proyecto3_bases;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by ferllini13 on 14/11/2016.
+ */
+
+public class SQLiteHandler extends SQLiteOpenHelper {
+    private static SQLiteHandler SQLiteHandler;
+
+    public SQLiteHandler(Context ctx){
+        super(ctx,"DB",null,1);
+    }
+    public void onCreate(SQLiteDatabase db) {
+        String query0="CREATE TABLE Usuario";
+        db.execSQL(query0);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+
+    }
+
+    public static SQLiteHandler getDB(Context context) {
+        if (SQLiteHandler == null){
+            SQLiteHandler = new SQLiteHandler(context);
+        }
+        return SQLiteHandler;
+    }
+
+    public void addUser() {
+        ContentValues values = new ContentValues();
+
+        values.put("Nombre", "");
+
+        insertData("tabla", values);
+    }
+
+    public void addProduct() {
+        ContentValues values = new ContentValues();
+
+        values.put("Nombre", "");
+
+        insertData("tabla", values);
+    }
+
+
+
+    public void addOrder() {
+        ContentValues values = new ContentValues();
+
+        values.put("Nombre","");
+
+        insertData("tabla", values);
+
+    }
+
+    public void addCategory(){
+        ContentValues values = new ContentValues();
+
+        values.put("Nombre","");
+
+        insertData("tabla", values);
+    }
+
+
+    public void insertData (String table  ,ContentValues content) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.insert(table, null, content);
+    }
+}
